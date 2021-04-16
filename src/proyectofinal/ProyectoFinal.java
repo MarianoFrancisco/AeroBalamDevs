bbdefd12a8af970221974036c1e2160c94b692fc
Mariano <mariano1941@outlook.es>
2021-04-16 13:58:05 -0600

    Creamos nuestra inicialización del proyecto de practica final.

---
+++ src/proyectofinal/ProyectoFinal.java
@@ -0,0 +1,14 @@
+package proyectofinal;
+/**
+ *
+ * @author Mariano
+ */
+public class ProyectoFinal {
+    /**
+     * Nuestra clase principal
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
